a4b4196bdd4a28db2962ee0b906300d5abbd886c
Amdadul Haque <76150397+Md-Amdadulhaque@users.noreply.github.com>
2024-11-14 01:10:28 +0600

    Add files via upload

---
+++ Subcategory.cs
@@ -0,0 +1,15 @@
+﻿namespace E_commerce.Models
+{
+    public class Subcategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category category { get; set; }
+        public Subcategory subcategory { get; set; }
+        public int parentId { get; set; }
+
+        public List<Product> Products = new List<Product>();
+
+    }
+}
